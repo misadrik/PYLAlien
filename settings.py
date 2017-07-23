0f68b349046c0258e75b39252be10f6ca9872528
qy19941014 <qian2006111@gmail.com>
2017-07-23 22:55:02 +0800

    update the init value

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ class Settings():
     'define all the related class'
     def __init__(self):
         #init all the settings
-        self.screen_width = 1200
-        self.screem_heigth = 800
+        self.screen_width = 720
+        self.screem_heigth = 640
         self.bg_color = (230,230,230)
