bbb012b822cb7d6f11e9e3443b3b0ce6580d793d
qy19941014 <qian2006111@gmail.com>
2017-07-23 23:33:10 +0800

    import game_functions.py

--- Aliens.py
+++ Aliens.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     #init and creat a screen object
@@ -16,10 +17,11 @@ def run_game():
     ship = Ship(screen)#creat a ship 
 
     while True:
-        #watch the keyboad and mouse
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        # #watch the keyboad and mouse
+        # for event in pygame.event.get():
+        #     if event.type == pygame.QUIT:
+        #         sys.exit()
+        gf.check_events()
 
         # refill the screen everytimegit remote add origin git@github.com:qy19941014/PYLAlien.git        
         screen.fill(ai_settings.bg_color)
