1b14ba5963715020708bb8be596c736e7c256d5e
qy19941014 <qian2006111@gmail.com>
2017-07-23 22:49:47 +0800

    set all the init settings

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    'define all the related class'
+    def __init__(self):
+        #init all the settings
+        self.screen_width = 1200
+        self.screem_heigth = 800
+        self.bg_color = (230,230,230)
