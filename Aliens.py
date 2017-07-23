623cec86baf5a901f78d8c5388ed90d27dd7fc4f
qy19941014 <qian2006111@gmail.com>
2017-07-23 22:04:46 +0800

    set the background color

--- Aliens.py
+++ Aliens.py
@@ -2,16 +2,23 @@ import sys
 import pygame
 
 def run_game():
-    pygame.init()
+    #init and creat a screen object
+    pygame.init() 
     screen = pygame.display.set_mode((720,640))
     pygame.display.set_caption("Alien Invasion")
+
+    bg_color = (230,230,230) #set the background color
     
     while True:
-        
+        #watch the keyboad and mouse
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        # refill the screen everytimegit remote add origin git@github.com:qy19941014/PYLAlien.git        
+        screen.fill(bg_color)
+
+        #display the screen
         pygame.display.flip()
 
 run_game()
