ccec2e83717e298a906ef9a58281094a964741c6
qy19941014 <qian2006111@gmail.com>
2017-07-24 15:04:52 +0800

    reconstruct check_event()

--- game_functions.py
+++ game_functions.py
@@ -2,23 +2,31 @@ import sys
 
 import pygame
 
+def key_down_events(event,ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def key_up_events(event,ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
+
 #watch the keyboad and mouse
 def check_events(ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
+        
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            key_down_events(event,ship)
 
         elif event.type ==pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            key_up_events(event,ship)
 
 #update the screen
 def update_screen(ai_settings,screen,ship):
